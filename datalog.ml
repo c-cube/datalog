bbcd43498d7767ecb2aab7d94097b5c511f6066c
Simon Cruanes <simon.cruanes.2007@m4x.org>
2012-12-24 17:29:43 +0100

    print progress while adding rules

--- datalog.ml
+++ datalog.ml
@@ -12,17 +12,23 @@ let parse_file filename =
   close_in ic;
   rules
 
+let pp_progress i total =
+  Format.printf "\r%% rule %-5d / %-5d  " i total;
+  Format.print_flush ()
+
 (** Compute fixpoint of rules *)
 let process_rules rules =
   Format.printf "process the following %d rules:@." (List.length rules);
   List.iter (Format.printf "  rule @[<h>%a@]@." (Logic.pp_rule ?to_s:None)) rules;
   Format.printf "@.computing fixpoint...@.";
   let db = Logic.db_create () in
-  List.iter (Logic.db_add db) rules;
+  (* add rules one by one *)
+  let total = List.length rules in
+  ignore (List.fold_left (fun i rule -> pp_progress i total; Logic.db_add db rule; i+1) 1 rules);
   Format.printf "done.@.";
+  (* print fixpoint of set after application of rules *)
   Logic.db_fold (fun () rule ->
     if Logic.is_fact rule then Format.printf "  @[<h>%a@]@." (Logic.pp_rule ?to_s:None) rule) () db
-
 
 let () =
   Format.printf "start datalog@.";

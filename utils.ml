6fcb96a94839b443404427e24e76ca4e77115004
Simon Cruanes <simon.cruanes.2007@m4x.org>
2012-12-25 02:36:04 +0100

    open addressing hashtable for integers. The purpose is to lower memory consumption of int hashtables, that are very numerous in the term index (flat discrimination tree) and to reduce the levels of indirection

--- utils.ml
+++ utils.ml
@@ -18,9 +18,6 @@ let murmur_hash i =
   let hash = hash lxor (hash lsr 15) in
   abs hash
 
-(** Efficient hashtable on ints *)
-module IHashtbl = Hashtbl.Make( struct type t = int let equal i j = i = j let hash i = murmur_hash i end)
-
 (** Sets of int *)
 module ISet = Set.Make(struct type t = int let compare i j = i - j end)
 
@@ -47,6 +44,145 @@ let hash_ints a =
     h := (!h + 65536) * murmur_hash a.(i);
   done;
   abs !h
+
+(* ----------------------------------------------------------------------
+ * Int hashtable
+ * ---------------------------------------------------------------------- *)
+
+(** Efficient hashtable on ints. It does open addressing with linear probing. *)
+module IHashtbl =
+  struct
+    type 'a t = {
+      mutable buckets : (int * 'a * state) array;
+      mutable size : int;
+    }
+    and state = Used | Empty | Deleted
+
+    let create size = {
+      buckets = Array.make size (0, Obj.magic None, Empty);
+      size = 0;
+    }
+
+    (** clear the table, by resetting all states to Empty *)
+    let clear t =
+      let rec clear_bucket i =
+        if i = Array.length t.buckets then ()
+          else (t.buckets.(i) <- (0, Obj.magic None, Empty); clear_bucket (i+1))
+      in
+      clear_bucket 0;
+      t.size <- 0
+
+    (** Insert (key -> value) in buckets, starting with the hash. *)
+    let insert buckets h key value =
+      let n = Array.length buckets in
+      (* lookup an empty slot to insert the key->value in. *)
+      let rec lookup i =
+        match buckets.(i) with
+        | (_, _, Empty) -> buckets.(i) <- (key, value, Used)
+        | (key', _, _) when key' = key -> ()
+        | _ -> lookup ((i+1) mod n)
+      in
+      lookup (h mod n)
+
+    (** Resize the array, by inserting its content into a twice as large array *)
+    let resize buckets =
+      let buckets' = Array.make (Array.length buckets * 2) (0, Obj.magic None, Empty) in
+      for i = 0 to Array.length buckets - 1 do
+        match buckets.(i) with
+        | (key, value, Used) ->
+          insert buckets' (murmur_hash key) key value  (* insert key -> value into new array *)
+        | _ -> ()
+      done;
+      buckets'
+
+    (** Lookup [key] in the table *)
+    let find t key =
+      let n = Array.length t.buckets in
+      let h = murmur_hash key in
+      let buckets = t.buckets in
+      let rec probe i num =
+        if num = n then raise Not_found
+        else match buckets.(i) with
+        | (key', value, Used) when key = key' ->
+          value  (* found value for this key *)
+        | (_, _, Deleted) | (_, _, Used) ->
+          probe ((i+1) mod n) (num + 1) (* try next bucket *)
+        | (_, _, Empty) -> raise Not_found
+      in
+      probe (h mod n) 0
+
+    (** Maximum ratio for (number of elements / number of buckets) *)
+    let max_load = 0.8
+
+    (** put [key] -> [value] in the hashtable *)
+    let replace t key value =
+      (if float_of_int t.size /. float_of_int (Array.length t.buckets) > max_load then t.buckets <- resize t.buckets);
+      let n = Array.length t.buckets in
+      let h = murmur_hash key in
+      let buckets = t.buckets in
+      let rec probe i =
+        match buckets.(i) with
+        | (key', _, Used) when key = key' ->
+          buckets.(i) <- (key, value, Used)  (* replace value *)
+        | (_, _, Deleted) |(_, _, Empty) ->
+          buckets.(i) <- (key, value, Used); t.size <- t.size + 1 (* insert and increment size *)
+        | (_, _, Used) ->
+          probe ((i+1) mod n) (* go further *)
+      in
+      probe (h mod n)
+
+    (** alias for replace *)
+    let add t key value = replace t key value
+
+    (** Remove the key from the table *)
+    let remove t key =
+      let n = Array.length t.buckets in
+      let h = murmur_hash key in
+      let buckets = t.buckets in
+      let rec probe i =
+        match buckets.(i) with
+        | (key', _, Used) when key = key' ->
+          buckets.(i) <- (0, Obj.magic None, Deleted); t.size <- t.size - 1  (* remove slot *)
+        | (_, _, Deleted) | (_, _, Used) ->
+          probe ((i+1) mod n) (* search further *)
+        | (_, _, Empty) -> ()  (* not present *)
+      in
+      probe (h mod n)
+
+    (** size of the table *)
+    let length t = t.size
+
+    (** Is the key member of the table? *)
+    let mem t key =
+      try ignore (find t key); true
+      with Not_found -> false
+
+    (** Iterate on key -> value pairs *)
+    let iter k t =
+      let buckets = t.buckets in
+      for i = 0 to Array.length buckets - 1 do
+        match buckets.(i) with
+        | (key, value, Used) -> k key value
+        | _ -> ()
+      done
+
+    (** Fold on key -> value pairs *)
+    let fold f t acc =
+      let acc = ref acc in
+      let buckets = t.buckets in
+      for i = 0 to Array.length buckets - 1 do
+        match buckets.(i) with
+        | (key, value, Used) -> acc := f key value !acc
+        | _ -> ()
+      done;
+      !acc
+      
+  end
+
+
+(*
+module IHashtbl = Hashtbl.Make( struct type t = int let equal i j = i = j let hash i = murmur_hash i end)
+*)
 
 (* ----------------------------------------------------------------------
  * Utils for parsing/lexing

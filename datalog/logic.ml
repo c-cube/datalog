06d80a3749c024fd2fdd9825573ef9c2dd9dde79
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-01-28 14:06:23 +0100

    added license prelude

--- datalog/logic.ml
+++ datalog/logic.ml
@@ -1,3 +1,28 @@
+(*
+Copyright (c) 2013, Simon Cruanes
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.  Redistributions in binary
+form must reproduce the above copyright notice, this list of conditions and the
+following disclaimer in the documentation and/or other materials provided with
+the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*)
+
 (** Representation for Datalog terms and rules, and the main algorithm *)
 
 (** Module type for logic *)
@@ -404,8 +429,8 @@ module Make(Symbol : SymbolType) = struct
 
       (** The term index. It is a trie with, at each node, a hashset
           of elements, plus a map symbol/var -> subtrie *)
-      type t = 
-      | Node of DataSet.t ref * t Utils.IHashtbl.t  
+      type t =
+      | Node of DataSet.t ref * t Utils.IHashtbl.t
 
       (** Indexed elements *)
       type elt = X.t
@@ -617,7 +642,7 @@ module Make(Symbol : SymbolType) = struct
            matches the fact *)
         RulesIndex.retrieve_generalizations
           (fun () rule' subst ->
-            if not (is_fact rule') then 
+            if not (is_fact rule') then
               (* rule' is not a fact, and
                  subst(rule'.body.(0)) = fact, remove the first element of the
                  body of rule', that makes a new rule *)
--- datalog/parser.mly
+++ datalog/parser.mly
@@ -1,3 +1,27 @@
+/*
+Copyright (c) 2013, Simon Cruanes
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.  Redistributions in binary
+form must reproduce the above copyright notice, this list of conditions and the
+following disclaimer in the documentation and/or other materials provided with
+the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 %{
 
@@ -46,7 +70,7 @@
 parse_file:
   | rules EOI { $1 }
 
-rules: 
+rules:
   | rule { let r = [$1] in reset_vars (); r }
   | rule rules { $1 :: $2 }
 
@@ -66,7 +90,7 @@ term:
 args:
   | const { [`Symbol $1] }
   | UPPER_WORD { [get_var $1] }
-  | const COMMA args { (`Symbol $1) :: $3 } 
+  | const COMMA args { (`Symbol $1) :: $3 }
   | UPPER_WORD COMMA args { (get_var $1) :: $3 }
 
 const:

8690cf9b40ec0bc2c59730cec620b62090b9a484
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-01-28 14:07:07 +0100

    actually need the _tags' <datalog>: include to build datalog_cli; added license prelude to datalog_cli.ml

--- Makefile
+++ Makefile
@@ -5,7 +5,8 @@ BINDIR ?= /usr/bin/
 NAME = datalog
 
 CLI = datalog_cli.native
-LIB = datalog.cmxa datalog.cma datalog.a datalog.cmi
+LIB = datalog.cmxa datalog.cma
+INSTALL_LIB = datalog.cmxa datalog.cma datalog.cmi datalog.mli
 
 # compilation options
 OPTIONS ?= -classic-display
@@ -13,19 +14,22 @@ OPTIONS ?= -classic-display
 all: prod
 
 prod:
-	ocamlbuild $(OPTIONS) -tag noassert $(LIB) $(CLI)
+	ocamlbuild $(OPTIONS) -tag noassert $(LIB)
+	ocamlbuild $(OPTIONS) -tag noassert -I _build/datalog/ $(CLI)
 
 debug:
 	ocamlbuild $(OPTIONS) -tag debug $(LIB) $(CLI)
+	ocamlbuild $(OPTIONS) -tag debug _build/$(LIB) $(CLI)
 
 profile:
 	ocamlbuild $(OPTIONS) -tag profile $(LIB) $(CLI)
+	ocamlbuild $(OPTIONS) -tag profile _build/$(LIB) $(CLI)
 
 clean:
 	ocamlbuild -clean
 
 install: prod
-	ocamlfind install $(NAME) META $(addprefix _build/,$(LIB))
+	ocamlfind install $(NAME) META $(addprefix _build/,$(INSTALL_LIB))
 	cp _build/$(CLI) $(BINDIR)/datalog_cli
 
 .PHONY: all clean install
--- datalog_cli.ml
+++ datalog_cli.ml
@@ -1,9 +1,34 @@
+(*
+Copyright (c) 2013, Simon Cruanes
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.  Redistributions in binary
+form must reproduce the above copyright notice, this list of conditions and the
+following disclaimer in the documentation and/or other materials provided with
+the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*)
+
 (** The main datalog file. It provides a CLI tool to parse rule/fact files and compute
     their fixpoint *)
 
-module DLogic = Datalog.Logic.Default
-module DParser = Datalog.Parser
-module DLexer = Datalog.Lexer
+module DLogic = Logic.Default
+module DParser = Parser
+module DLexer = Lexer
 
 let progress = ref false
 let print_input = ref false
@@ -54,10 +79,10 @@ let process_rules rules =
   (* print fixpoint of set after application of rules *)
   (if !print_size then
     Format.printf "%% size of saturated set: %d@." (DLogic.db_size db));
-  (if !print_saturated then 
+  (if !print_saturated then
     DLogic.db_fold (fun () rule ->
       Format.printf "  @[<h>%a@]@." DLogic.pp_rule rule) () db
-  else if !print_result then 
+  else if !print_result then
     DLogic.db_fold (fun () rule ->
       if DLogic.is_fact rule then
         Format.printf "  @[<h>%a@]@." DLogic.pp_rule rule) () db);

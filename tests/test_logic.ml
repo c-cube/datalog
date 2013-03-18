c2583e6a195515f9e506e29a5cab7056ebb16e22
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-03-18 01:05:42 +0100

    more tests

--- tests/test_logic.ml
+++ tests/test_logic.ml
@@ -9,6 +9,9 @@ module L = Datalog.Logic.Default
 let parse_lit str =
   Datalog.Parser.parse_literal Datalog.Lexer.token (Lexing.from_string str)
 
+let parse_clause str =
+  Datalog.Parser.parse_clause Datalog.Lexer.token (Lexing.from_string str)
+
 let are_alpha_equiv lit1 lit2 =
   try
     ignore (L.alpha_equiv (lit1,0) (lit2,1));
@@ -23,7 +26,42 @@ let test_parse_lit () =
   OUnit.assert_bool "alpha_renaming" (are_alpha_equiv lit lit');
   ()
 
+let test_hashconsing () =
+  let lit1 = parse_lit "p(X,Y,Z)" in
+  let lit2 = parse_lit "p(X,Y,Z)" in
+  OUnit.assert_equal ~cmp:(==) lit1 lit2;
+  let lit3 = L.mk_apply "foo" [lit1; lit1] in
+  let lit4 = L.mk_apply "foo" [lit2; lit2] in
+  OUnit.assert_equal ~cmp:(==) lit3 lit4;
+  OUnit.assert_bool "not_eq" (lit1 != lit3);
+  ()
+
+let test_ground () =
+  OUnit.assert_bool "not ground" (not (L.is_ground (parse_lit "p(X,a,b)")));
+  OUnit.assert_bool "ground" (L.is_ground (parse_lit "p(a,b,'c',42,d)"));
+  OUnit.assert_bool "ground" (L.is_ground (parse_lit "q(r(p(a,b),c),d)"));
+  ()
+
+let test_arity () =
+  OUnit.assert_equal 3 (L.arity (parse_lit "p(a,b,c)"));
+  OUnit.assert_equal 1 (L.arity (parse_lit "p(a)"));
+  OUnit.assert_equal 0 (L.arity (parse_lit "X"));
+  ()
+
+let test_safe () =
+  let c = parse_clause "p(a,b,c)." in
+  OUnit.assert_bool "safe" (L.check_safe c);
+  let c' = parse_clause "r(X,Y) :- r(X,Z), rr(Z,Y)." in
+  OUnit.assert_bool "safe" (L.check_safe c');
+  let c'' = parse_clause "r(X,Y,Z) :- p(X), q(Y), fail(a)." in
+  OUnit.assert_bool "not safe" (not (L.check_safe c''));
+  ()
+
 let suite =
   "test_logic" >:::
     [ "test_parse_lit" >:: test_parse_lit;
+      "test_hashconsing" >:: test_hashconsing;
+      "test_ground" >:: test_ground;
+      "test_arity" >:: test_arity;
+      "test_safe" >:: test_safe;
     ]
